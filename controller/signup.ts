1cb8482b18825dee2e1434b74adc2a568bf3eb2b
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-15 09:20:53 +0900

    [modify] 회원가입 시 예외 처리, 로그인 페이지로 리다이렉트

--- controller/signup.ts
+++ controller/signup.ts
@@ -18,18 +18,20 @@ async function get(req: Request, res: Response) {
 //회원가입 Post
 async function post(req: Request, res: Response) {
   const { username, password } = req.body;
-  const result = await db.user.create({
-    username,
-    password,
-  });
-  const { id } = result.toJSON();
-  req.session.user = id; // 세션에 사용자 정보 저장
   try {
-    res.send({
-      result: true,
-      name: username,
-      password: password,
+    const result = await db.user.create({
+      username,
+      password,
     });
+    const { id } = result.toJSON();
+    req.session.user = id; // 세션에 사용자 정보 저장
+  } catch (e) {
+    console.log("아이디 중복", e);
+    res.send({ result: false });
+    return;
+  }
+  try {
+    res.redirect("/login");
   } catch (err) {
     console.log("회원가입 실패", err);
     res.send({ result: false });
