8caa00490193ba907914759f5020504cafe6c1fb
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-22 00:04:27 +0900

    [Modify] getLoginSession 수정
    사용 시 마다 새로운 USER 생성

--- utils/testutil.ts
+++ utils/testutil.ts
@@ -16,6 +16,11 @@ export function login(id: string, pw: string, app: Express) {
 }
 
 export async function getLoginSession(id: string, pw: string, app: Express) {
+  try {
+    await signup(id, pw, app);
+  } catch (e) {
+    console.log(e);
+  }
   return (await login(id, pw, app)).header["set-cookie"];
 }
 
