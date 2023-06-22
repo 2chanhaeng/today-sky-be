1a6195618944f380e5177899028fd21fd0312c60
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-22 14:08:02 +0900

    [Create] Unauthorized 에러 생성
    기존에는 Access 토큰이 없을 시 에러로 간주했다.
    Unauthorized 에러를 추가하여 토큰이 없을 경우 비로그인 상태로 처리하도록 만들었다.
    해당 예외는 오류 출력 없이 그대로 반환한다.

--- utils/login.ts
+++ utils/login.ts
@@ -3,11 +3,12 @@ import jwt from "jsonwebtoken";
 import db from "@/models";
 import config from "@/config/token";
 import { getFromDB } from "./getDB";
+import { Unauthorized } from "@/types/error";
 
 export default async function isLogin(req: Request, res: Response) {
   try {
     const access = req.headers.authorization || req.cookies.access;
-    if (!access) throw new Error("로그인 되어 있지 않음");
+    if (!access) throw new Unauthorized("");
     // access 토큰이 존재하는 경우
     const { id } = jwt.verify(access, config.ACCESS_TOKEN) as jwt.JwtPayload;
     if (!id) throw new Error("유저 정보 없음");
@@ -16,6 +17,11 @@ export default async function isLogin(req: Request, res: Response) {
     if (err instanceof jwt.TokenExpiredError) {
       // access 토큰이 만료된 경우에만 refresh 토큰 검증
       console.log("Access 토큰 만료");
+    } else if (err instanceof Unauthorized) {
+      // Access 없을 경우 비 로그인 상태로 취급
+      // 별다른 경고 없이 그냥 리턴
+      // 로그인이 필요한 경우 로그인 페이지로 리디렉션
+      return;
     } else {
       // 그외의 경우 모두 로그인 페이지로 리디렉션
       console.log("Access 토큰 검증 오류", err);
