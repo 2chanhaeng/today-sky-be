f9bda54c7df80be1ee77facd9b1a85e04e45c7f3
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-22 20:58:41 +0900

    [Modify] COMMENT 삭제 테스트 생성

--- __tests__/comment.ts
+++ __tests__/comment.ts
@@ -91,3 +91,39 @@ test("update comment", async () => {
   // DB에서 불러온 코멘트 내용이 일치하는지 확인
   expect(dbComment?.dataValues.content).toBe(content);
 });
+
+test("delete comment", async () => {
+  // username, password를 생성
+  const [username, password] = genIdPw();
+  // 유저로 로그인을 하고 쿠키(로그인 세션) 추출
+  const cookie = await getLoginCookies(username, password, app);
+  // 쿠키로 유저 아이디를 추출
+  const user_id = getUserIDfromCookie(cookie)!;
+  // 임의의 날짜로 오늘 날짜를 가져옴
+  const [year, month, date] = today();
+  // 임의의 투두 생성
+  const todo = await db.todo.create({
+    year,
+    month,
+    date,
+    user_id,
+    content: genString(),
+  });
+  // 투두 아이디 추출
+  const todo_id = todo.dataValues.id;
+  // 임의의 내용으로 코멘트 생성
+  const comment = await db.comment.create({ todo_id, content: genString() });
+  if (!comment) throw new Error("comment 생성 실패");
+  // API로 코멘트 삭제
+  const res = await request(app)
+    .delete(url(todo_id))
+    .set("Cookie", cookie)
+    .send({ todo_id });
+  // 삭제 결과를 받아옴
+  const result = res.body.result as boolean;
+  // 삭제 결과가 true인지 확인
+  expect(result).toBe(true);
+  // DB에서 코멘트를 불러와 null인지 확인
+  const dbComment = await db.comment.findOne({ where: { todo_id } });
+  expect(dbComment).toBeNull();
+});
