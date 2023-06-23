a6a59eb1816cf70dd210d13f140a4d32f75f72dc
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-24 02:26:05 +0900

    [Refac] logout Prisma 적용

--- controller/logout.ts
+++ controller/logout.ts
@@ -1,15 +1,22 @@
 import { Request, Response } from "express";
 import db from "@/db";
-import isLogin from "@/utils/login";
+import { isLogin, sendOrLogErrorMessage } from "@/utils";
+import { BadRequest, Unauthorized } from "@/types/error";
 
 export default {
   get,
 };
 
 async function get(req: Request, res: Response) {
-  const id = await isLogin(req, res);
-  if (!id) return res.redirect("/login");
-  const result = await db.user.update({ refresh: "" }, { where: { id } });
-  if (!result) return res.status(500).json({ message: "로그아웃 실패" });
-  res.clearCookie("access").clearCookie("refresh").redirect("/");
+  try {
+    const id = await isLogin(req, res);
+    if (!id) throw new Unauthorized("Not Login");
+    const where = { id };
+    const data = { refresh: null };
+    const result = await db.user.update({ where, data });
+    if (!result) throw new BadRequest("User does not exist");
+    res.clearCookie("access").clearCookie("refresh").redirect("/");
+  } catch (error) {
+    sendOrLogErrorMessage(res, error);
+  }
 }
