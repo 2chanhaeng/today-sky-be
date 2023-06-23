a94340d6e7ee3b5b1ad34bed8f87945db79c2f39
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-24 03:19:03 +0900

    [Delete] 사용하지 않는 파일 제거

--- prisma/migrations/20230623132611_add_todo_unique_has_todo/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[id,user_id]` on the table `Todo` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Todo_id_user_id_key` ON `Todo`(`id`, `user_id`);
