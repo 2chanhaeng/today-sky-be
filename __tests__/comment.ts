8349d8e5ac1e31e5d9014dfd4ceab65b20fd785c
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-24 02:00:59 +0900

    [Refac] COMMENT DELETE 테스트 Prisma 적용

--- __tests__/comment.ts
+++ __tests__/comment.ts
@@ -55,38 +55,21 @@ test("update comment", async () => {
 });
 
 test("delete comment", async () => {
-  // username, password를 생성
-  const [username, password] = genIdPw();
-  // 유저로 로그인을 하고 쿠키(로그인 세션) 추출
-  const cookie = await getLoginCookies(username, password, app);
-  // 쿠키로 유저 아이디를 추출
-  const user_id = getUserIDfromCookie(cookie)!;
-  // 임의의 날짜로 오늘 날짜를 가져옴
-  const [year, month, date] = today();
-  // 임의의 투두 생성
-  const todo = await db.todo.create({
-    year,
-    month,
-    date,
-    user_id,
-    content: genString(),
-  });
-  // 투두 아이디 추출
-  const todo_id = todo.dataValues.id;
+  // 투두를 생성하고 투두 아이디와 쿠키를 추출
+  const { cookie, todo_id } = await createTodoAndCookie();
   // 임의의 내용으로 코멘트 생성
-  const comment = await db.comment.create({ todo_id, content: genString() });
+  const data = { todo_id, content: genString() };
+  const comment = await db.comment.create({ data });
   if (!comment) throw new Error("comment 생성 실패");
   // API로 코멘트 삭제
   const res = await request(app)
-    .delete(url(todo_id))
+    .post(url(todo_id))
     .set("Cookie", cookie)
     .send({ todo_id });
-  // 삭제 결과를 받아옴
-  const result = res.body.result as boolean;
   // 삭제 결과가 true인지 확인
-  expect(result).toBe(true);
+  expect(res.body).toBe(true);
   // DB에서 코멘트를 불러와 null인지 확인
-  const dbComment = await db.comment.findOne({ where: { todo_id } });
+  const dbComment = await db.comment.findUnique({ where: { todo_id } });
   expect(dbComment).toBeNull();
 });
 
