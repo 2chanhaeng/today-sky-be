b071d365948cad84c6bcc2a73e66d300cd6012ab
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-23 17:01:46 +0900

    [Create] ConnectionError

--- types/error.ts
+++ types/error.ts
@@ -1,6 +1,16 @@
-export class Unauthorized extends Error {
+/** 서버 내 에러를 처리하기 위한 추상적 예외 */
+export class ConnectionError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.message = `${this.name}: ${message}`;
+  }
+  status = 500;
+}
+
+export class Unauthorized extends ConnectionError {
   constructor(message: string) {
     super(message);
     this.name = "Unauthorized";
   }
+  status = 401;
 }
