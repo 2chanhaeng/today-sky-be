a596a58de72a283854faa1a0f727d6f089c5d5c0
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-24 02:00:47 +0900

    [Refac] COMMENT UPDATE 테스트 Prisma 적용

--- __tests__/comment.ts
+++ __tests__/comment.ts
@@ -33,42 +33,25 @@ test("create comment", async () => {
 });
 
 test("update comment", async () => {
-  // username, password를 생성
-  const [username, password] = genIdPw();
-  // 유저로 로그인을 하고 쿠키(로그인 세션) 추출
-  const cookie = await getLoginCookies(username, password, app);
-  // 쿠키로 유저 아이디를 추출
-  const user_id = getUserIDfromCookie(cookie)!;
-  // 임의의 날짜로 오늘 날짜를 가져옴
-  const [year, month, date] = today();
-  // 임의의 투두 생성
-  const todo = await db.todo.create({
-    year,
-    month,
-    date,
-    user_id,
-    content: genString(),
-  });
-  // 투두 아이디 추출
-  const todo_id = todo.dataValues.id;
+  // 투두를 생성하고 투두 아이디와 쿠키를 추출
+  const { cookie, todo_id } = await createTodoAndCookie();
   // 임의의 내용으로 코멘트 생성
-  const comment = await db.comment.create({ todo_id, content: genString() });
+  const data = { todo_id, content: genString() };
+  const comment = await db.comment.create({ data });
   if (!comment) throw new Error("comment 생성 실패");
   // 업데이트할 내용 생성
   const content = genString();
   // API로 코멘트 업데이트
   const res = await request(app)
-    .put(url(todo_id))
+    .post(url(todo_id))
     .set("Cookie", cookie)
     .send({ todo_id, content });
-  // 업데이트된 코멘트를 받아옴
-  const result = res.body.result as boolean;
-  // 업데이트 결과가 true인지 확인
-  expect(result).toBe(true);
+  // 결과가 true인지 확인
+  expect(res.body).toBe(true);
   // DB에서 업데이트된 코멘트를 불러옴
-  const dbComment = await db.comment.findOne({ where: { todo_id } });
+  const dbComment = await db.comment.findUnique({ where: { todo_id } });
   // DB에서 불러온 코멘트 내용이 일치하는지 확인
-  expect(dbComment?.dataValues.content).toBe(content);
+  expect(dbComment?.content).toBe(content);
 });
 
 test("delete comment", async () => {
