1ff943a42cce68f566237004edb6b9968c893819
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-24 01:33:08 +0900

    [Delete] COMMENT PUT → POST 통합
    upsert 로 통합

--- controller/comment.ts
+++ controller/comment.ts
@@ -37,37 +37,6 @@ async function post(req: Request, res: Response) {
   }
 }
 
-// comment수정
-async function put(req: Request, res: Response) {
-  try {
-    const user_id = await isLogin(req, res);
-    if (!user_id) return res.redirect("/login");
-    const todo_id = Number(req.params.todo_id);
-    const todo = await db.todo.findOne({ where: { user_id, id: todo_id } });
-    if (!todo) {
-      return res
-        .status(404)
-        .json({ message: "Todo가 존재하지 않음.", result: false });
-    }
-    const { content } = req.body;
-    const comment = await db.comment.findOne({ where: { todo_id } });
-    if (!comment) {
-      return res
-        .status(404)
-        .json({ message: "Comment가 존재하지 않음.", result: false });
-    }
-    const result = await db.comment.update({ content }, { where: { todo_id } });
-    if (!result) {
-      return res
-        .status(500)
-        .json({ message: "Comment 수정 실패.", result: false });
-    }
-    res.status(200).json({ message: "Comment 수정 완료.", result: true });
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error", result: false });
-  }
-}
-
 // comment 삭제
 async function destroy(req: Request, res: Response) {
   try {
