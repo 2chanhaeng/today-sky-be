533aebd6f4b12d45da0c399507d0bf5d4837e4a3
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-26 02:54:25 +0900

    [Modify] COMMENT CREATE, UPDATE 테스트 EMOTION 추가

--- __tests__/comment.ts
+++ __tests__/comment.ts
@@ -7,6 +7,7 @@ import {
   genPort,
   getUserIDfromCookie,
   genString,
+  genEmoji,
 } from "@/utils/testutil";
 import setPort from "@/testapp";
 
@@ -19,38 +20,42 @@ test("create comment", async () => {
   const { cookie, todo_id } = await createTodoAndCookie();
   // 임의의 코멘트 내용을 생성
   const content = genString();
+  const emotion_id = genEmoji();
   // API로 코멘트 생성
   const res = await request(app)
     .post(url(todo_id))
     .set("Cookie", cookie)
-    .send({ todo_id, content });
+    .send({ todo_id, content, emotion_id });
   expect(res.body).toBe(true);
   // TODO ID로 생성된 코멘트를 DB에서 불러옴
   const dbComment = await db.comment.findUnique({ where: { todo_id } });
   // DB에서 불러온 코멘트 내용이 일치하는지 확인
   expect(dbComment?.content).toBe(content);
+  expect(dbComment?.emotion_id).toBe(emotion_id);
 });
 
 test("update comment", async () => {
   // 투두를 생성하고 투두 아이디와 쿠키를 추출
   const { cookie, todo_id } = await createTodoAndCookie();
   // 임의의 내용으로 코멘트 생성
-  const data = { todo_id, content: genString() };
+  const data = { todo_id, content: genString(), emotion_id: genEmoji() };
   const comment = await db.comment.create({ data });
   if (!comment) throw new Error("comment 생성 실패");
   // 업데이트할 내용 생성
   const content = genString();
+  const emotion_id = genEmoji();
   // API로 코멘트 업데이트
   const res = await request(app)
     .post(url(todo_id))
     .set("Cookie", cookie)
-    .send({ todo_id, content });
+    .send({ todo_id, content, emotion_id });
   // 결과가 true인지 확인
   expect(res.body).toBe(true);
   // DB에서 업데이트된 코멘트를 불러옴
   const dbComment = await db.comment.findUnique({ where: { todo_id } });
   // DB에서 불러온 코멘트 내용이 일치하는지 확인
   expect(dbComment?.content).toBe(content);
+  expect(dbComment?.emotion_id).toBe(emotion_id);
 });
 
 test("delete comment", async () => {
