a2b438833bce456d4879d11b0da9f9667ec2f99c
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-01-12 16:41:30 +0900

    feat: create diary gets controller

--- src/diary/diary.controller.ts
+++ src/diary/diary.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Diary, PrismaService } from '~/prisma/prisma.service';
 
 @Controller('diary')
-export class DiaryController {}
+export class DiaryController {
+  constructor(private readonly prismaService: PrismaService) {}
+  @Get()
+  async gets(): Promise<Diary[]> {
+    return this.prismaService.diary.findMany();
+  }
+}
--- src/diary/diary.module.ts
+++ src/diary/diary.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DiaryService } from './diary.service';
 import { DiaryController } from './diary.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  providers: [DiaryService],
+  providers: [PrismaService, DiaryService],
   controllers: [DiaryController],
 })
 export class DiaryModule {}
