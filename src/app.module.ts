c802e1d3b1b8955ef2bd7993b918c28b2c4b451c
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-01-12 17:38:31 +0900

    feat: create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DiaryModule } from './diary/diary.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DiaryModule, PrismaModule],
+  imports: [DiaryModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
