a53787b3797f9cdd936207706a5f36905dc7175f
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-01-12 18:07:15 +0900

    feat: create signup controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { pbkdf2 } from '@/utils/auth';
+import { PrismaService } from '~/prisma/prisma.service';
+import { CreateUserBodyDto } from './dto/createUser.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Post('signup')
+  async signup(@Body() { username, password }: CreateUserBodyDto) {
+    return this.prismaService.user.create({
+      data: { ...pbkdf2(password), username },
+    });
+  }
+}
